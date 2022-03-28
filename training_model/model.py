5da6a94b89a056848056d215b45b0ee280854128
chuangcaleb <balechuang@gmail.com>
2022-03-28 14:12:39 +0100

    refactor: Sampling dataset

--- training_model/model.py
+++ training_model/model.py
@@ -1,6 +1,7 @@
 
 # *  Import Dataset
 
+from collections import Counter
 from sklearn.model_selection import train_test_split
 from mtr_utils import config as cfg
 
@@ -10,6 +11,7 @@ from mtr_utils.label_dataset_selection import extractLabelDataset
 
 from mtr_utils.feature_selection.load_feature_set import preselected_feature_list
 from mtr_utils.feature_selection.auto_feature_selection import filterVarianceThreshold
+from mtr_utils.sampling import undersample, oversample, smote
 
 
 # * Extract data from label dataset
@@ -22,3 +24,16 @@ manual_feature_df = raw_feature_df[preselected_feature_list]
 
 selected_feature_np = filterVarianceThreshold(
     manual_feature_df, cfg.threshold_val)
+
+# * Splitting Dataset
+
+feature_np = selected_feature_np
+label_np = label_df[['risk']].to_numpy().astype(int)
+
+(x_train, x_test, y_train, y_test) = train_test_split(
+    feature_np, label_np, test_size=0.2, random_state=cfg.rand_state)
+
+# * Sampling
+
+X_resampled, y_resampled = smote(x_train, y_train, cfg.rand_state)
+print(sorted(Counter(y_resampled).items()))
