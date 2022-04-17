d6bf7b4c2ae96eedb708919e7e73858263ac6cea
chuangcaleb <balechuang@gmail.com>
2022-04-18 00:51:46 +0100

    refac: graph.py to use config.json

--- evaluating_results/graph.py
+++ evaluating_results/graph.py
@@ -4,31 +4,33 @@ from eval_utils import config as cfg
 
 label_stats_dict = json.load(open('data/labels/label_stats_summary.json', "r"))
 std_results_dict = json.load(
-    open(cfg.OUTPUT_PATH + 'results/best_results.json', "r"))
+    open(cfg.OUTPUT_PATH + 'results/results_best.json', "r"))
+config_dict = json.load(
+    open(cfg.OUTPUT_PATH + 'run_config.json', "r"))
 
-label_stats_list = [key for key, value in label_stats_dict['%'].items()]
-label_stats_list.reverse()
+# label_stats_list = [key for key, value in label_stats_dict['%'].items()]
+# label_stats_list.reverse()
+label_stats_list = config_dict['SELECTED_LABELS']
 
 color_list = ['r', 'b', 'g', 'c', 'm', 'y']
 
-# print(std_results_dict['risk'])
-
-# clf_results = dict.fromkeys(std_results_dict)
-
-clf_list = ['GaussianNB', 'kNN', 'SVM', 'DecnTree', 'RandForest']
+clf_list = config_dict['ACTUAL_CLASSIFIERS']
 clf_results = {k: [] for k in clf_list}
 
 for label, clf_dict in std_results_dict.items():
 
     for clf, scores_dict in clf_dict.items():
+
         if clf not in clf_list:
             continue
 
-        clf_results[clf].append(scores_dict['accura'])
+        clf_results[clf].append(scores_dict['rocauc'])
 
 plt.title("Standard Deviation by Positive Label Proportion")
 plt.xlabel("Positive Label Proportion (%)")
 plt.ylabel("Standard Deviation")
+
+print(clf_results)
 
 for i, clf in enumerate(clf_list):
     plt.plot(label_stats_list,
