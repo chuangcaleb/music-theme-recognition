a4981594cfa328bc18826ac28145b02b151e4c1d
chuangcaleb <balechuang@gmail.com>
2022-04-21 19:58:11 +0100

    Checkpoint dummy commit

--- evaluating_results/eval_utils/results_stats_utils.py
+++ evaluating_results/eval_utils/results_stats_utils.py
@@ -6,14 +6,15 @@ from eval_utils import load_results as data
 current_classifiers = [clf['name'] for clf in data.config_dict['CLASSIFIERS']]
 
 
-def calc_stats(results_dict, row_headers, match_clf):
+def calc_stats(results_dict, row_headers, table_ids, match_clf):
 
     stat_dict = {}
 
     # For each table id, build a table
     for label, seed_dict in results_dict.items():
+        # for label, seed_dict in results_dict.items():
 
-        label_stat_dict = get_label_stat(seed_dict, row_headers, match_clf)
+        label_stat_dict = get_label_stats(seed_dict, row_headers, match_clf)
 
         # We build the output dict label-by-label
         stat_dict.update({label: label_stat_dict})
@@ -23,11 +24,9 @@ def calc_stats(results_dict, row_headers, match_clf):
     return stat_dict
 
 
-def get_label_stat(seed_dict, row_headers, match_clf=True):
+def get_label_stats(seed_dict, row_headers, match_clf=True):
 
     row_ids = row_headers.keys()
-    # row_funcs = list(row_headers.values())
-    # print(row_funcs)
 
     # init template dict of metrics
     scores_template_dict = {k: [] for k in data.config_dict['METRICS']}
@@ -46,9 +45,10 @@ def get_label_stat(seed_dict, row_headers, match_clf=True):
                 if clf not in row_ids:
                     continue  # Skip baseline classifiers
 
-                for score, value in score_dict.items():
+                for metric, score in score_dict.items():
 
-                    label_stat_dict[clf][score].append(value)
+                    # Collect a list of all relevant scores
+                    label_stat_dict[clf][metric].append(score)
 
     else:
 
@@ -56,18 +56,23 @@ def get_label_stat(seed_dict, row_headers, match_clf=True):
 
             for clf, score_dict in clf_dict.items():
 
-                for score, value in score_dict.items():
+                for metric, score in score_dict.items():
 
-                    [label_stat_dict[v][score].append(value) for v in row_ids]
+                    # Append for each row
+                    [label_stat_dict[v][metric].append(score) for v in row_ids]
 
-    # Average each list item
+    # Apply function to each list item
     for row, score_dict in label_stat_dict.items():
 
-        for score, values in score_dict.items():
+        for metric, values in score_dict.items():
 
-            # print(row_funcs[row](values))
-            # # print(label_stat_dict[row][score])
-
-            label_stat_dict[row][score] = row_headers[row](values)
+            # Get current function
+            func = row_headers[row]
+            # Apply function
+            label_stat_dict[row][metric] = func(values)
 
     return(label_stat_dict)
+
+
+def get_classifier_stats():
+    pass
--- evaluating_results/results_stats.py
+++ evaluating_results/results_stats.py
@@ -2,10 +2,11 @@ from statistics import mean, stdev
 
 from eval_utils import load_results as data
 from eval_utils.results_stats_utils import calc_stats
-from eval_utils.results_stats_utils import get_label_stat
+from eval_utils.results_stats_utils import get_label_stats
 from eval_utils.export_eval import dump_results
 
 current_classifiers = [clf for clf in data.config_dict['ACTUAL_CLASSIFIERS']]
+current_labels = [clf for clf in data.config_dict['SELECTED_LABELS']]
 
 average_clf = {clf: mean for clf in current_classifiers}
 stdev_clf = {clf: stdev for clf in current_classifiers}
@@ -28,3 +29,6 @@ dump_results(std_results_dict, 'results_std', 'Standard Deviation')
 
 label_results_dict = calc_stats(data.all_results_dict, stats, match_clf=False)
 dump_results(label_results_dict, 'results_labels', 'Label Overview')
+
+model_results_dict = calc_stats(data.all_results_dict, stats, match_clf=False)
+dump_results(label_results_dict, 'results_models', 'Model Overview')
