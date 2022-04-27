f5dea09c0d5c07c02325e53080fe4f27da15296a
chuangcaleb <balechuang@gmail.com>
2022-04-27 15:07:44 +0100

    refac: load feature set with bin sizes from definition

--- building_model/mtr_utils/feature_selection/load_feature_set.py
+++ building_model/mtr_utils/feature_selection/load_feature_set.py
@@ -1,64 +1,35 @@
 """ Loads the list of manually shortlisted features """
 
+from typing import List
+import pandas as pd
 import json
 
-# Path is relative to building_model instead of root, because the model.ipynb is run relative to itself and not root.
-json_file_path = 'building_model/mtr_utils/feature_selection/feature_set.json'
-# output_file_path = 'building_model/mtr_utils/feature_selection/manually_preselected_features.txt'
+JSON_PATH = 'building_model/mtr_utils/feature_selection/feature_set.json'
+FT_DEF_PATH = 'data/features/song_theme_feature_definitions.csv'
+
+ft_set_dict = json.loads(open(JSON_PATH).read())
+ft_def_df = pd.read_csv('data/features/song_theme_feature_definitions.csv')
 
 
-# def load_presel_features(json_file_path, output_file_path):
-def load_presel_features(json_file_path):
-    """ Loads and parses the json preselected feature set and returns it a list
-     of strings """
-
-    features_file = json.loads(
-        open(json_file_path).read()
-    )
-
-    preselected_feature_list = []
-
-    # For each category in the file
-    for category_name in features_file:
-
-        # Process category only if true, skip otherwise
-        if features_file[category_name]['category_enabled'] == True:
-
-            # Get category object
-            current_category = features_file[category_name]['features']
-
-            # For each feature in current category
-            for feature_name in current_category:
-
-                # Get feature object
-                current_feature = current_category[feature_name]
-
-                # If feature is enabled, skip otherwise
-                if current_feature['feature_enabled'] == True:
-
-                    # If bin_size exists, create a list of features
-                    if 'bin_size' in current_feature:
-                        current_feature_range = [
-                            feature_name + '_' + str(x) for x in range(current_feature['bin_size'])
-                        ]
-                        # Append range of features to main list
-                        preselected_feature_list.extend(current_feature_range)
-                    # Else, just add the single feature
-                    else:
-                        # Append single feature to main list
-                        preselected_feature_list.append(feature_name)
-
-    # Write output to .txt file too
-    # with open(output_file_path, 'w') as f:
-    #     for feature in preselected_feature_list:
-    #         f.write(feature + '\n')
-
-    # print('\nSuccessfully written manually preselected features to ' + output_file_path + '\n')
-    print(
-        f'\nSucessfully loaded {len(preselected_feature_list)} manually selected features.')
-
-    return(preselected_feature_list)
+def explode_bins(feature_str: str) -> List[List[str]]:
+    feature_bin_count = ft_def_df.loc[ft_def_df.id ==
+                                      feature_str]['parallel_dimensions'].values[0]
+    if feature_bin_count > 1:
+        return [feature_str + '_' + str(x) for x in range(feature_bin_count)]
+    else:
+        return [feature_str]
 
 
-preselected_feature_list = load_presel_features(json_file_path)
-# json_file_path, output_file_path)
+def flatten(lst: str) -> List[str]:
+    return (item for sublist in lst for item in sublist)
+
+
+preselected_feature_list = flatten([
+    explode_bins(feature)
+
+    for category in ft_set_dict.values()
+    if category['category_enabled'] is True
+
+    for feature, value in category['features'].items()
+    if value['feature_enabled'] is True
+])
