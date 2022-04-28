10d3058df54cdbe1f5894754d07305a0f42975cf
chuangcaleb <balechuang@gmail.com>
2022-04-28 10:16:26 +0100

    catch: Add try/except/else in import_dataset

--- building_model/mtr_utils/import_dataset.py
+++ building_model/mtr_utils/import_dataset.py
@@ -1,3 +1,4 @@
+from logging import raiseExceptions
 import pandas as pd
 from mtr_utils import config as cfg
 
@@ -12,6 +13,13 @@ try:
     # Access song_theme_labels_database
     raw_label_df = pd.read_excel(LABEL_DB_PATH)
 
+except Exception as e:
+
+    print('\nThere was an error in importing the datasets.')
+    print(e)
+
+else:
+
     # Extract recognizable data from label dataset
     recognz_label_df = raw_label_df[raw_label_df.recognizable == 1]
 
@@ -22,7 +30,3 @@ try:
     data_label_df = extracted_label_df.drop('id', axis=1)
 
     print('\nSucessfully imported music theme recognition dataset.')
-
-except:
-
-    print('\nThere was an error in importing the datasets.')
